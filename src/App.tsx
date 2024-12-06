28faac51372cf3cfbe22f491e7240af95e7fd142
YedixonRDev <yedixonjrf@gmail.com>
2024-12-06 13:36:03 -0500

    config ts

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from './components/ui/button';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Button className="bg-blue-300 text-white hover:bg-blue-800">Click Me</Button>
+    </>
+  )
+}
+
+export default App
